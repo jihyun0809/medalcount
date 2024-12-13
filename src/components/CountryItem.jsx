1f5a8986f1fec2b5d5fe197c3269735d175f994e
jiji <pagwa334@gmail.com>
2024-12-14 01:01:42 +0900

    💄리셋 css 추가, ✅  삭제버튼 추가

--- src/components/CountryItem.jsx
+++ src/components/CountryItem.jsx
@@ -1,17 +1,14 @@
-const CountryItem = ({ country }) => {
-  //한개를 표시하는 컴포넌트
+const CountryItem = ({ country, onClick }) => {
   return (
-    <>
-      <tr key={country.name}>
-        <td>{country.name}</td>
-        <td>{country.gold}</td>
-        <td>{country.silver}</td>
-        <td>{country.bronze}</td>
-        <td>
-          <button>삭제!</button>
-        </td>
-      </tr>
-    </>
+    <tr>
+      <td>{country.name}</td>
+      <td>{country.gold}</td>
+      <td>{country.silver}</td>
+      <td>{country.bronze}</td>
+      <td>
+        <button onClick={() => onClick(country.name)}>삭제</button>
+      </td>
+    </tr>
   );
 };
 
